817a193038871ca8f135696aaf0bff68646431f4
DcoolerGuy <joao.penna@sptech.school>
2022-11-24 00:32:39 -0300

    adição da tela de leilão e mudanças no bd

--- bd/tabelas.sql
+++ bd/tabelas.sql
@@ -5,9 +5,7 @@ create table usuario(
 idusuario int primary key auto_increment,
 nome varchar(45),
 email varchar(45),
-senha char(10),
-fkferrari int,
-foreign key(fkferrari) references ferrari(idferrari)
+senha char(10)
 );
 
 select*from usuario;
@@ -23,15 +21,32 @@ ano int
 
 create table leilao(
 idleilao int auto_increment,
+fkproprietario int,
+foreign key (fkproprietario) references usuario(idusuario),
 fkferrari int,
-foreign key (fkferrari) references ferrari(idferrari),
-valorInicial decimal(10,2)
+foreign key(fkferrari) references ferrari(idferrari),
+primary key(idleilao,fkproprietario,fkferrari),
+valorInicial decimal(10,2),
+fkvencedor int,
+foreign key (fkvencedor) references usuario(idusuario)
 );
+alter table leilao modify column valorinicial decimal(13,2);
+alter table leilao modify column lancefinal decimal(13,2);
+
+insert into usuario values
+(null,'João Penna','joao.penna@sptech.school','senha1'),
+(null,'Fernanda Caramico','fernanda.caramico@sptech.school','senha2');
+select*from usuario;
 
 insert into ferrari values
-(null,'458 italia','Rosso Corsa',2015),
-(null,'458 italia','Rosso Corsa',2015),
-(null,'458 italia','Rosso Corsa',2015),
-(null,'458 italia','Rosso Corsa',2015),
-(null,'458 italia','Rosso Corsa',2015),
-(null,'458 italia','Rosso Corsa',2015),
+(null,'f40','rosso-corsa',1991),
+(null,'laferari','rosso-corsa',2014);
+select*from ferrari;
+
+insert into leilao values
+(null,1,1,12000000,null,null),
+(null,2,2,102000000,null,null);
+select*from leilao;
+
+
+
